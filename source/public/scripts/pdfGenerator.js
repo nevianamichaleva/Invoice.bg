ee6e4c315acff216a6cd737b6e209d757382128d
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-12-02 21:06:46 +0200

    refactoring

--- source/public/scripts/pdfGenerator.js
+++ source/public/scripts/pdfGenerator.js
@@ -1,34 +1,34 @@
 /* globals require module $ jsPDF domtoimage Image window*/
-
 "use strict";
-console.log('i was here');
 var PDFGenerator = (function(){
-    //<button class="btn btn-primary btn-lg login-button" id="save-invoice" type="button">Запис</button>
-    const invoiceClassName = '.big-invoice';
-    const buttonGenerateClassName = '#save-invoice'
-    console.log("I'm in")
+    const invoiceClassName = '.big-invoice',
+          buttonGenerateId = '#save-invoice',
+          pdfView = 'landscape',
+          imageType = 'PNG',
+          pdfFileName = 'invoice.pdf';
     return {
         generatePdf : function(){
-            var $button = $(buttonGenerateClassName);
+            var $button = $(buttonGenerateId);
             var $elementToBeConverted = $(invoiceClassName)[0];
-            //console.log($button.html);
             $button.on("click", function(){
-                var doc = new jsPDF('landscape');
-                console.log('I clicked');
-                domtoimage.toBlob($elementToBeConverted).then(function(blob) {
-                    var urlCreator = window.URL || window.webkitURL;
-                    var imageUrl = urlCreator.createObjectURL(blob);
-                    var img = new Image();
-                    img.src = imageUrl;
-                    console.log('i here in blob');
-                    img.onload = function() {
-                        doc.addImage(img, 'PNG', 0 , 0 , 300, 200);
-                        doc.save("invoice.pdf");
-                    };
-                });
-            });
-        }
-    };
+                var doc = new jsPDF(pdfView);
+                domtoimage
+                    .toBlob($elementToBeConverted)
+                    .then(function(blob) {
+                        var urlCreator = window.URL || window.webkitURL;
+                                var imageUrl = urlCreator.createObjectURL(blob);
+                                var img = new Image();
+                                img.src = imageUrl;
+                       // return Promise.resolve(
+                            //new Promise(function (resolve){
+                                img.onload = function(){                                    
+                                    doc.addImage(img, imageType, 0, 0, 300, 200);
+                                    doc.save(pdfFileName);
+                                }
+                            });
+                       
+        });
+    }}
 })();
 
 PDFGenerator.generatePdf();
