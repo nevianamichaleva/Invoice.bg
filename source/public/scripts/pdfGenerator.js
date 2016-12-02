d2301341215e08d27464e6c5a84f03fd1043f3d0
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-12-02 19:58:11 +0200

    pdf converter with new button id

--- source/public/scripts/pdfGenerator.js
+++ source/public/scripts/pdfGenerator.js
@@ -1,25 +1,26 @@
-/* globals require module $*/
-/* globals require module jsPDF*/
-/* globals require module domtoimage*/
-/*globals require module Image*/
+/* globals require module $ jsPDF domtoimage Image window*/
+
 "use strict";
-
+console.log('i was here');
 var PDFGenerator = (function(){
+    //<button class="btn btn-primary btn-lg login-button" id="save-invoice" type="button">Запис</button>
     const invoiceClassName = '.big-invoice';
-    const buttonGenerateClassName = 'a.btn.btn-primary.btn-lg.login-button'
-
+    const buttonGenerateClassName = '#save-invoice'
+    console.log("I'm in")
     return {
         generatePdf : function(){
             var $button = $(buttonGenerateClassName);
             var $elementToBeConverted = $(invoiceClassName)[0];
-            console.log($elementToBeConverted);
+            //console.log($button.html);
             $button.on("click", function(){
-                var doc = new jsPDF('landscape');       
+                var doc = new jsPDF('landscape');
+                console.log('I clicked');
                 domtoimage.toBlob($elementToBeConverted).then(function(blob) {
                     var urlCreator = window.URL || window.webkitURL;
                     var imageUrl = urlCreator.createObjectURL(blob);
                     var img = new Image();
                     img.src = imageUrl;
+                    console.log('i here in blob');
                     img.onload = function() {
                         doc.addImage(img, 'PNG', 0 , 0 , 300, 200);
                         doc.save("invoice.pdf");
